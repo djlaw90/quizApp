cf9bbc23bad152b755522b25995600177ee278d3
djlaw90 <djlaw90@Gmail.com>
2020-07-06 22:27:08 -0500

    added start button + js functionality

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+
+
+
+
+
+var startButton = document.getElementById("start-button");
+var questionBox = document.getElementById("question-box")
+
+var showQuiz = () => { questionBox.classList.remove("hidden"); startButton.classList.add("hidden") };
+
+startButton.addEventListener("click", showQuiz);
