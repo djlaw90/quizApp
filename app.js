8a869944974e56bb75d5ad1be7d195663265e5c5
djlaw90 <djlaw90@Gmail.com>
2020-07-09 15:49:58 -0500

    added question number header and number correct/incorrect header

--- app.js
+++ app.js
@@ -4,8 +4,17 @@
 
 
 var startButton = document.getElementById("start-button");
-var questionBox = document.getElementById("question-box")
+var questionBox = document.getElementById("question-box");
+var questionCounter = document.getElementById("question-counter");
+var scoreBox = document.getElementById("score");
 
-var showQuiz = () => { questionBox.classList.remove("hidden"); startButton.classList.add("hidden") };
+
+//shows counter and question box, hides the start button
+var showQuiz = () => {
+  questionBox.classList.remove("hidden");
+  questionCounter.classList.remove("hidden");
+  scoreBox.classList.remove("hidden");
+  startButton.classList.add("hidden");
+ };
 
 startButton.addEventListener("click", showQuiz);
