23e1532e189319e62f1ae8a076c95577591048cd
djlaw90 <djlaw90@Gmail.com>
2020-07-13 23:15:57 -0500

    pulling question from object

--- app.js
+++ app.js
@@ -3,10 +3,25 @@
 
 
 
-var startButton = document.getElementById("start-button");
-var answerBox = document.getElementById("answer-box");
-var infoDisplay = document.getElementById("info-box");
-var question = document.getElementById("question");
+const startButton = document.getElementById("start-button");
+const answerBox = document.getElementById("answer-box");
+const infoDisplay = document.getElementById("info-box");
+const questionElement = document.getElementById("question");
+
+let shuffledQuestions, currentQuestionIndex;
+
+const questions = [
+  {
+    question: "Who wasn't in Super Smash Bros. for N64?",
+    answers: [
+      {text: "Mario", correct: false},
+      {text: "Waluigi", correct: true},
+      {text: "Yoshi", correct: false},
+      {text: "Captain Falcon", correct: false}
+    ]
+
+  }
+];
 
 //shows counter and question box, hides the start button
 var showQuiz = () => {
@@ -14,6 +29,18 @@ var showQuiz = () => {
   infoDisplay.classList.remove("hidden");
   question.classList.remove("hidden");
   startButton.classList.add("hidden");
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0;
+  setNextQuestion();
  };
 
 startButton.addEventListener("click", showQuiz);
+
+
+var setNextQuestion = () => {
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
+
+var showQuestion = (question) => {
+  questionElement.innerText = question.question;
+};
