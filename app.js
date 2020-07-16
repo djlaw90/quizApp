765db0a4f5d64eaec4aef5756174fbd2b5f8b4ef
djlaw90 <djlaw90@Gmail.com>
2020-07-15 23:21:44 -0500

    answers populate now

--- app.js
+++ app.js
@@ -14,13 +14,31 @@ const questions = [
   {
     question: "Who wasn't in Super Smash Bros. for N64?",
     answers: [
-      {text: "Mario", correct: false},
-      {text: "Waluigi", correct: true},
-      {text: "Yoshi", correct: false},
-      {text: "Captain Falcon", correct: false}
+      { text: "Mario", correct: false },
+      { text: "Waluigi", correct: true },
+      { text: "Yoshi", correct: false },
+      { text: "Captain Falcon", correct: false }
     ]
-
+  },
+  {
+    question: "What was Mario's OG name?",
+    answers: [
+      { text: "Jumpman", correct: true },
+      { text: "Superdude", correct: false },
+      { text: "Highguy", correct: false },
+      { text: "Mario", correct: false }
+    ]
+  },
+  {
+    question: "What did Bowser used to be called?",
+    answers: [
+      { text: "Koopa", correct: false },
+      { text: "Troopa", correct: false },
+      { text: "Lakitu", correct: false },
+      { text: "King Koopa", correct: true }
+    ]
   }
+
 ];
 
 //shows counter and question box, hides the start button
@@ -43,4 +61,19 @@ var setNextQuestion = () => {
 
 var showQuestion = (question) => {
   questionElement.innerText = question.question;
+  question.answers.forEach(answer => {
+    //Creates new paragraph element for each answer, prepends answer HTML to the answer box
+    const p = document.createElement('p');
+
+    const answerDiv =
+    `<div class="radio">
+      <label>
+        <input type="radio" name="answer"  value="option1" checked>
+        <p class="answer">${answer.text}</p>
+      </label>
+    </div>`;
+
+    p.innerHTML = answerDiv;
+    answerBox.prepend(p);
+  });
 };
